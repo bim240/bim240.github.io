19ca76d641c3f6147a01bc793dee5793b7381e78
bim240 <bimlendu357@gmail.com>
2020-01-17 12:32:08 +0530

    initial page commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Bimlendu's Blog`,
+    author: `Bimlendu kumar`,
+    description: `This is a trial blog`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/bim240`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/bim240`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    <Styled.a href="http://twitter.com/bim240">Bimlendu Kuamr</Styled.a>
+    {` `}
+    <br />
+    @bim240
+  </Fragment>
+)
